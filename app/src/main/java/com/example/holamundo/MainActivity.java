b04a7c6e0e2901c0c38f000ca1d9889ccb6b0356
luise-phd <luis.ordonez.palacios@correounivalle.edu.co>
2021-10-27 12:25:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/holamundo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Método onCreate()", Toast.LENGTH_LONG).show();
+        Log.i("Información", "Esto es una Prueba");
+    }
+
+    public void goToActivityProducto(View view) {
+        Intent newIntent = new Intent(this, ProductoActivity.class);
+        newIntent.putExtra("msg", "Hola");
+        newIntent.putExtra("year", 2021);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(newIntent);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Método onStart()", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Método onResume()", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Método onPause()", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Método onStop()", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "Método onRestart()", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Información", "Método onDestroy()");
+    }
+}
---
+++ app/src/main/java/com/example/holamundo/ProductoActivity.java
@@ -0,0 +1,46 @@
+package com.example.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class ProductoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producto);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getParametros();
+    }
+
+    public void goToActivityMain(View view) {
+        Intent newIntent = new Intent(this, MainActivity.class);
+        newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(newIntent);
+    }
+
+    public void getParametros() {
+        Bundle extras = getIntent().getExtras();
+        String msg = extras.getString("msg");
+        Integer year = extras.getInt("year");
+        Toast.makeText(this, msg + " " + year, Toast.LENGTH_SHORT).show();
+    }
+
+    public void OnBackPressed() {
+        finish();
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+
+        if(id == android.R.id.home) {
+            OnBackPressed();
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+}
