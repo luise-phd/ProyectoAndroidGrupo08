52b0121edf12fc28cd9cef9b0a75576aeb18b60b
luise-phd <luis.ordonez.palacios@correounivalle.edu.co>
2021-11-17 13:43:43 -0500

    Almacenamiento y recuperación de imagenes en SQLite, Poblar un spinner dinamicamente con datos de una base de datos

--- app/src/main/java/com/ingluise/ProyectoAndroidGrupo08/CamaraActivity.java
+++ app/src/main/java/com/ingluise/ProyectoAndroidGrupo08/CamaraActivity.java
@@ -1,14 +1,18 @@
 package com.ingluise.ProyectoAndroidGrupo08;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -42,6 +46,7 @@ public class CamaraActivity extends AppCompatActivity {
         txtDescripcion = findViewById(R.id.txtDescripcion);
         btnCamara = findViewById(R.id.btnCamara);
         imgView = findViewById(R.id.imageView);
+        imgView.setImageDrawable(null);
         btnCamara.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,37 +83,75 @@ public class CamaraActivity extends AppCompatActivity {
         if(id == R.id.mnu_guardar) {
             String des = txtDescripcion.getText().toString();
             if (!des.equals("")) {
-                String imgCodificada = "";
-                imgView.buildDrawingCache(true);
-                bmp = imgView.getDrawingCache(true);
+                if (imgView.getDrawable() != null) {
+                    String imgCodificada = "";
+                    imgView.buildDrawingCache(true);
+                    bmp = imgView.getDrawingCache(true);
 
-                bmp2 = Bitmap.createScaledBitmap(bmp, imgView.getWidth(), imgView.getHeight(), true);
+                    bmp2 = Bitmap.createScaledBitmap(bmp, imgView.getWidth(), imgView.getHeight(), true);
 
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bmp2.compress(Bitmap.CompressFormat.JPEG, 25, baos);
-                byte[] imagen = baos.toByteArray();
-                imgCodificada = Base64.encodeToString(imagen, Base64.DEFAULT);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bmp2.compress(Bitmap.CompressFormat.PNG, 25, baos);
+                    byte[] imagen = baos.toByteArray();
+                    imgCodificada = Base64.encodeToString(imagen, Base64.DEFAULT);
 
-                //Almacenar los datos
-                db = admin.getWritableDatabase();
-                cv = new ContentValues();
-                cv.put("descripcion", des);
-                cv.put("img", imgCodificada);
-                long reg = db.insert("imagenes", null, cv);
-                if (reg != -1) {
-                    Toast.makeText(this, "Registro almacenado", Toast.LENGTH_SHORT).show();
-                    txtDescripcion.setText("");
-                    imgView.setImageBitmap(null);
-                    imgView.destroyDrawingCache();
-                    imgView.setImageDrawable(null);
-                } else
-                    Toast.makeText(this, "El registro no se pudo almacenar", Toast.LENGTH_SHORT).show();
-            } else {
+                    //Almacenar los datos
+                    db = admin.getWritableDatabase();
+                    cv = new ContentValues();
+                    cv.put("descripcion", des);
+                    cv.put("img", imgCodificada);
+                    long reg = db.insert("imagenes", null, cv);
+                    if (reg != -1) {
+                        Toast.makeText(this, "Registro almacenado", Toast.LENGTH_SHORT).show();
+                        txtDescripcion.setText("");
+                        imgView.setImageBitmap(null);
+                        imgView.destroyDrawingCache();
+                        imgView.setImageDrawable(null);
+                    } else
+                        Toast.makeText(this, "El registro no se pudo almacenar", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(this, "Por favor, capture la fotografía", Toast.LENGTH_SHORT).show();
+            } else
                 Toast.makeText(this, "Por favor, ingrese la descripción", Toast.LENGTH_SHORT).show();
-            }
         }
         else if(id == R.id.mnu_buscar) {
-
+            LayoutInflater li = LayoutInflater.from(this);
+            View promptsView = li.inflate(R.layout.input_dialog, null);
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setView(promptsView);
+            final EditText et1 = promptsView.findViewById(R.id.input_buscar_img);
+            alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Bitmap decodedBytes = null;
+                            byte[] decodeString;
+                            db = admin.getWritableDatabase();
+                            filas = db.rawQuery("SELECT * FROM imagenes WHERE descripcion='"+et1.getText().toString()+"'", null);
+                            String des = "";
+                            if (filas.moveToFirst()) {
+                                des = filas.getString(1);
+                                if (!filas.getString(2).equals("")) {
+                                    decodeString = Base64.decode(filas.getString(2), Base64.DEFAULT);
+                                    decodedBytes = BitmapFactory.decodeByteArray(decodeString, 0, decodeString.length);
+                                }
+                                txtDescripcion.setText(des);
+                                imgView.setImageBitmap(decodedBytes);
+                            } else
+                                Toast.makeText(getApplicationContext(), "El registro no existe", Toast.LENGTH_SHORT).show();
+                            db.close();
+                        }
+                    })
+                    .setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
         }
 
         return super.onOptionsItemSelected(menuItem);
---
+++ app/src/main/java/com/ingluise/ProyectoAndroidGrupo08/FacturaActivity.java
@@ -0,0 +1,65 @@
+package com.ingluise.ProyectoAndroidGrupo08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.MergeCursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class FacturaActivity extends AppCompatActivity {
+    private MyDBSQLiteHelper admin;
+    private SQLiteDatabase db;
+    private Cursor filas;
+
+    private EditText et1, et2;
+    private Spinner sp1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_factura);
+
+        admin = new MyDBSQLiteHelper(this, vars.nomDB, null, vars.version);
+
+        et1 = findViewById(R.id.txt_codigo);
+        et2 = findViewById(R.id.txt_fecha);
+        sp1 = findViewById(R.id.sp_productos);
+
+        db = admin.getReadableDatabase();
+        filas = db.rawQuery("SELECT _id, (nombre || ' - ' || marca) AS nom FROM producto ORDER BY nombre", null);
+        MatrixCursor extras = new MatrixCursor(new String[]{"_id", "nom"});
+        extras.addRow(new String[]{"-1", "Seleccione..."});
+        Cursor[] cursores = {extras, filas};
+        Cursor cursorExtendido = new MergeCursor(cursores);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                this, android.R.layout.simple_spinner_dropdown_item,
+                cursorExtendido, new String[]{"nom"}, new int[]{android.R.id.text1},
+                SimpleCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp1.setAdapter(adapter);
+        db.close();
+
+        sp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String producto = cursorExtendido.getString(cursorExtendido.getColumnIndex("nom"));
+                Toast.makeText(getApplicationContext(), i+"-"+producto, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
