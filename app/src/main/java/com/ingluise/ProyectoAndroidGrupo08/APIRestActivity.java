1b932a9567297ed8d592af3e9fa8e0f996fa77d7
luise-phd <luis.ordonez.palacios@correounivalle.edu.co>
2021-12-01 14:20:13 -0500

    Lectura de archivo Json desde una API Rest

--- app/src/main/java/com/ingluise/ProyectoAndroidGrupo08/APIRestActivity.java
+++ app/src/main/java/com/ingluise/ProyectoAndroidGrupo08/APIRestActivity.java
@@ -7,11 +7,17 @@ import android.os.Bundle;
 import android.util.JsonReader;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -27,25 +33,18 @@ public class APIRestActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    URL githubEndpoint = new URL("https://api.github.com/");
+                    URL githubEndpoint = new URL("https://www.datos.gov.co/resource/xdk5-pm3f.json");
                     HttpsURLConnection myConnection = (HttpsURLConnection) githubEndpoint.openConnection();
-                    myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
-                    myConnection.setRequestProperty("Accept", "application/vnd.github.v3+json");
-                    myConnection.setRequestProperty("Contact-Me", "usuario@gmail.com");
                     if (myConnection.getResponseCode() == 200) {
                         Log.e(TAG, "Conexión exitosa");
                         InputStream responseBody = myConnection.getInputStream();
                         InputStreamReader responseBodyReader = new InputStreamReader(responseBody, "UTF-8");
                         JsonReader jsonReader = new JsonReader(responseBodyReader);
-                        jsonReader.beginObject();
-                        while (jsonReader.hasNext()) {
-                            String key = jsonReader.nextName();
-                            if (key.equals("current_user_url")) {
-                                String value = jsonReader.nextString();
-                                Log.e(TAG, value);
-                                break;
-                            } else {
-                                jsonReader.skipValue();
+                        List<String> listado = readMessagesArray(jsonReader);
+                        for (int i=0; i<listado.size(); i++) {
+                            String[] municipio = listado.get(i).split("- Municipio: ");
+                            if (municipio[1].equals("Bochalema")) {
+                                Log.i(TAG, listado.get(i));
                             }
                         }
                         jsonReader.close();
@@ -60,5 +59,48 @@ public class APIRestActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public List<String> readMessagesArray(JsonReader reader) throws IOException {
+        List<String> messages = new ArrayList<String>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            messages.add(readMessage(reader));
+        }
+        reader.endArray();
+        return messages;
+    }
+
+    public String readMessage(JsonReader reader) throws IOException {
+        String region = "";
+        String c_digo_dane_del_departamento = "";
+        String departamento = "";
+        String c_digo_dane_del_municipio = "";
+        String municipio = "";
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("region")) {
+                region = reader.nextString();
+            } else if (name.equals("c_digo_dane_del_departamento")) {
+                c_digo_dane_del_departamento = reader.nextString();
+            } else if (name.equals("departamento")) {
+                departamento = reader.nextString();
+            } else if (name.equals("c_digo_dane_del_municipio")) {
+                c_digo_dane_del_municipio = reader.nextString();
+            } else if (name.equals("municipio")) {
+                municipio = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return new String("Region: " + region + " - " +
+                "Cod. Departamento: " + c_digo_dane_del_departamento + " - " +
+                "Departamento: " + departamento + " - " +
+                "Cod. Municipio: " + c_digo_dane_del_municipio + " - " +
+                "Municipio: " + municipio);
     }
 }
